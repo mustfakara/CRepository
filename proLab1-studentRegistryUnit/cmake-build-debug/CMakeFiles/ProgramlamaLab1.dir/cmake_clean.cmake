95db762fe13dc1aa1ad12cd5ae6bc98461ea4907
Mustafa Kara <88894240+mustfakara@users.noreply.github.com>
2021-10-29 21:47:51 +0300

    Add files via upload

---
+++ proLab1-studentRegistryUnit/cmake-build-debug/CMakeFiles/ProgramlamaLab1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgramlamaLab1.dir/main.c.obj"
+  "ProgramlamaLab1.exe"
+  "ProgramlamaLab1.exe.manifest"
+  "ProgramlamaLab1.pdb"
+  "libProgramlamaLab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProgramlamaLab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
